d7a8458dcac6f7c7007f35b4d36819d5fcee62b7
prixus <prixus@github.com>
2021-03-15 17:42:05 +0800

    Images setup

--- first_django_project/settings.py
+++ first_django_project/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = BASE_DIR / 'assets'
+
+# Path for media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,23 +3,5 @@ from .models import MenuItem
 
 # Create your views here.
 def index(request):
-    menu_item_1 = MenuItem()
-    menu_item_1.name = 'Salad'
-    menu_item_1.description = 'A healthier choice'
-    menu_item_1.img = 'img/gallery/01.jpg'
-    menu_item_1.price = 45
-
-    menu_item_2 = MenuItem()
-    menu_item_2.name = 'Peperoni Pizza'
-    menu_item_2.description = 'Taste from Italy'
-    menu_item_2.img = 'img/gallery/02.jpg'
-    menu_item_2.price = 70
-
-    menu_item_3 = MenuItem()
-    menu_item_3.name = 'Bread'
-    menu_item_3.description = 'A piece of the meditteranian'
-    menu_item_3.img = 'img/gallery/03.jpg'
-    menu_item_3.price = 30
-
-    menu_items = [menu_item_1, menu_item_2, menu_item_3]
+    menu_items = MenuItem.objects.all()
     return render(request, 'index.html', {'menu_items': menu_items})
